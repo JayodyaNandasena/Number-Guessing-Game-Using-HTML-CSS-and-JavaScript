9355fbd702a6f5315bcf4b486da1020f15cde26a
Jayodya Nandasena <jayodyanandasena@gmail.com>
2024-03-02 10:52:53 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-let generateNum=()=>{
-    let rNum=Math.round(Math.random()*10) ;
+let generateNum = () => {
+    let rNum = Math.round(Math.random() * 10);
     console.log(rNum);
     return rNum;
 }
 
-var gNum=generateNum();
+var gNum = generateNum();
 
-let checkNum=(rNum)=>{
-    rNum=gNum;
+let count = 0;
+let btn = document.getElementById("btnSubmit");
+let disp = document.getElementById("display");
 
-    let num=document.getElementById("inNum").value;
+btnSubmit.onclick = function () {
+    count++;
+    let rNum = gNum;
 
-    if (rNum<num) {
-        alert("Guess a lesser number!");
-    } else if (rNum>num) {
-        alert("Guess a higher number!");
-    }else{
-        alert("Congragulations! You Win!!");
+    if (count<3) {
+        disp.innerHTML = count;
+        
+        let num = document.getElementById("inNum").value;
+    
+        if (rNum < num) {
+            alert("Guess a lesser number!");
+        } else if (rNum > num) {
+            alert("Guess a higher number!");
+        } else {
+            alert("Congragulations! You Win!!");
+            document.getElementById("btnSubmit").disabled = true;
+        } 
+    } else {
+        disp.innerHTML = count;
+        document.getElementById("btnSubmit").disabled = true;
+        alert("Maximum Number of Attempts Exceeded, the number is "+rNum);
     }
-
-
 }
