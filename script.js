1807d2c7b8e6ab59e15b78730b32c90c43d0ed28
Jayodya Nandasena <jayodyanandasena@gmail.com>
2024-03-02 19:16:29 +0530

    Adding Styles

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.getElementById("btnAgain").disabled = true;
+document.getElementById("message").style.visibility = "hidden";
+
 let generateNum = () => {
     let rNum = Math.round(Math.random() * 10);
     console.log(rNum);
@@ -13,25 +16,40 @@ let disp = document.getElementById("display");
 btnSubmit.onclick = function () {
     count++;
     let rNum = gNum;
+    let num = document.getElementById("inNum").value;
+    let isGuessed=false;
 
-    if (count<3) {
-        attempts.innerHTML = count;
-        left.innerHTML = 3-count;
-        
-        let num = document.getElementById("inNum").value;
-    
+    attempts.innerHTML = count;
+    left.innerHTML = 3 - count;
+
+    if (count < 3) {
         if (rNum < num) {
+            document.getElementById("message").style.visibility = "visible";
             message.innerHTML = "Guess a lesser number!";
         } else if (rNum > num) {
+            document.getElementById("message").style.visibility = "visible";
             message.innerHTML = "Guess a higher number!";
         } else {
-            message.innerHTML = "Congragulations! You Win!!";
+            document.getElementById("message").style.visibility = "visible";
+            message.innerHTML = "Congragulations! You Won!!";
             document.getElementById("btnSubmit").disabled = true;
-        } 
-    } else {
-        attempts.innerHTML = count;
-        left.innerHTML = 3-count;
+            isGuessed=true;
+        }
+    } else if (count == 3 && rNum == num) {
+        message.innerHTML = "Congragulations! You Won!!";
         document.getElementById("btnSubmit").disabled = true;
-        message.innerHTML = "Maximum Number of Attempts Exceeded, the number is "+rNum;
+    } else {
+        document.getElementById("btnSubmit").disabled = true;
+        message.innerHTML = "Maximum Number of Attempts Exceeded, the number is " + rNum;
+    }
+
+
+    if (count==3 || isGuessed==true) {
+        document.getElementById("btnAgain").disabled = false;
+        // const btnAgain = document.createElement('button');
+        // btnAgain.textContent = 'Play Again';
+        // document.body.appendChild(btnAgain);
     }
 }
+
+
